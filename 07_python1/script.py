a6ced4d0192a67b9c963f10b9df295c4049097a0
k.erushnikov <overmind0101@gmail.com>
2023-04-04 19:15:07 +0500

    script.py updated in \07_python1

--- 07_python1/script.py
+++ 07_python1/script.py
@@ -1,17 +1,63 @@
 from sys import argv
 import base64
 
-script, method, text = argv
+def code():
+    if int(len(argv)) == 3:
+        if argv[1] == "crypt":
+            text = argv[2].encode('ascii')
+            print("Encrypting...")
+            text = base64.b64encode(text)
+            print(text.decode('ascii'))
+        elif argv[1] == "decrypt":
+            text = argv[2].encode('ascii')
+            print("Decrypting...")
+            text = base64.b64decode(text)
+            print(text.decode('ascii'))
+        else:
+            print("Incorrect method")
+            argv[1] = str(input("Type crypt or decrypt: "))
+            if argv[1] == "crypt":
+                text = argv[2].encode('ascii')
+                print("Encrypting...")
+                text = base64.b64encode(text)
+                print(text.decode('ascii'))
+            elif argv[1] == "decrypt":
+                text = argv[2].encode('ascii')
+                print("Decrypting...")
+                text = base64.b64decode(text)
+                print(text.decode('ascii'))
 
-if method == "crypt":
-    text = text.encode('ascii')
-    print("Encrypting...")
-    text = base64.b64encode(text)
-    print(text.decode('ascii'))
-elif method == "decrypt":
-    text = text.encode('ascii')
-    print("Decrypting...")
-    text = base64.b64decode(text)
-    print(text.decode('ascii'))
-else:
-    print("Wrong method input")
+    else:
+        print("Wrong arguments")
+        argv[1] = str(input("Type crypt or decrypt: "))
+        argv[2] = str(input("Type message: "))
+        if argv[1] == "crypt":
+                text = argv[2].encode('ascii')
+                print("Encrypting...")
+                text = base64.b64encode(text)
+                print(text.decode('ascii'))
+        elif argv[1] == "decrypt":
+            text = argv[2].encode('ascii')
+            print("Decrypting...")
+            text = base64.b64decode(text)
+            print(text.decode('ascii'))
+
+code()
+
+while True:
+    try:
+        flag = input('Ones again? [Y/N]: ')
+        if flag == "Y":
+            argv[1] = str(input("Type crypt or decrypt: "))
+            argv[2] = str(input("Type message: "))
+            code()
+        elif flag !="N":
+            print('Stupid? Type Y or N!')
+        else:
+            print("As u wish" "\n"
+                  "Quiting..." "\n")
+            break
+    except ValueError as err:
+        print(err)
+    except KeyboardInterrupt:
+        print("Press N in another iteration")
